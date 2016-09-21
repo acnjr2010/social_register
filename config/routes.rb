1e8d46a6e2792703180082fd27c15d409f37abe5
Junior <nog.junior84@gmail.com>
2016-09-21 18:12:20 -0300

    Configuração omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: 'home#index'
 
+  # rotas para o login com rede social
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
